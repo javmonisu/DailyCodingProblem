6caf467ddd83d3dc3061db3d144ccb4bead0386b
Javier Montero <javmonisu@gmail.com>
2019-08-28 18:30:29 +0200

    Problem 20190827 with division

--- src/problem_20190827/Problem20190827.java
+++ src/problem_20190827/Problem20190827.java
@@ -4,10 +4,23 @@ import java.util.Arrays;
 
 public class Problem20190827 {
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(productAllNumbersWithDivision(new int[]{1, 2, 3, 4, 5})));
+        System.out.println(Arrays.toString(productAllNumbersWithDivision(new int[]{3, 2, 1})));
         System.out.println(Arrays.toString(productAllNumbersArray(new int[]{1, 2, 3, 4, 5})));
         System.out.println(Arrays.toString(productAllNumbersArray(new int[]{3, 2, 1})));
     }
 
+    private static int[] productAllNumbersWithDivision(int[] nums) {
+        int[] products = new int[nums.length];
+        int sum = 1;
+        for (int num : nums) {
+            sum *= num;
+        }
+        for (int i = 0; i < products.length; i++) {
+            products[i] = sum / nums[i];
+        }
+        return products;
+    }
     private static int[] productAllNumbersArray(int[] nums) {
         int[] products = new int[nums.length];
         if (nums.length < 2) {
